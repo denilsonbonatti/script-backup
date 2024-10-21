d36821edf3c8254c0fcefc0e431b223f469ac90e
DENILSON LUIS BONATTI <denilson.lbonatti@senacsp.edu.br>
2024-10-21 16:22:01 -0300

    V. 1.0

---
+++ backup.bat
@@ -0,0 +1,5 @@
+mkdir backup
+copy /windows/*.txt /users/denilson.bonatti/documents/script-backup/backup
+copy /windows/system32/*.log /users/denilson.bonatti/documents/script-backup/backup
+copy /windows/system32/*.txt /users/denilson.bonatti/documents/script-backup/backup
+
